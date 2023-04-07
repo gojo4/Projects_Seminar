864b46ecb0836cc7e6a4a3c23e65c0ef79c5c46a
Nikita Robsky <kalipso,get-chief@yandex.ru>
2023-04-07 21:37:50 +0300

    fix: the program for finding     max of 3 numbers

---
+++ Task002_Maximum/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    if(a > c)
+    {
+    Console.WriteLine("maximum: " + a);
+    }
+    else
+    Console.WriteLine("maximum: " + c);
+}   
+if(b > a)
+{
+    if(b > c)
+    {
+        Console.WriteLine("maximum: " + b);
+    }
+    else
+    Console.WriteLine("maximum: " + c);
+}
