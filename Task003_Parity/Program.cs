34e9e6ad0832c68aff6b3c28fa6e658e192134e7
Nikita Robsky <kalipso,get-chief@yandex.ru>
2023-04-07 21:53:33 +0300

    fix: parity check program

---
+++ Task003_Parity/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 1)
+    {
+    Console.WriteLine("Нечетное");       
+    }
+        else
+        {
+        Console.WriteLine("Четное");
+        }пше 
