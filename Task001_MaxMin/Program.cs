50f0474785095d28c276b0fd0213c25653e9c67a
Nikita Robsky <kalipso,get-chief@yandex.ru>
2023-04-07 21:14:03 +0300

    fix: program choice of two numbers max and min

---
+++ Task001_MaxMin/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+    Console.WriteLine("Maxsimum: " + a + ";" + " Minimum: " + b);
+    else
+    Console.WriteLine("Maxsimum: " + b + "Minimum: " + a);
+
