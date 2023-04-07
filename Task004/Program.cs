069f4fefb815f15f99c452855e1cd56c38169aa4
Nikita Robsky <kalipso,get-chief@yandex.ru>
2023-04-07 22:31:39 +0300

    fix: the program for finding even numbers

---
+++ Task004/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int i = 1;
+bool m = true;
+Console.WriteLine("Введите N:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Чётные числа от 1 до " + N);
+while(i <= N)
+    {
+        if (i % 2 == 0){
+        Console.Write(i + ",");
+        m = false;
+        } 
+        i++;  
+    }
+        if (m){ 
+            Console.WriteLine("Четных чисел нет");
+            }
