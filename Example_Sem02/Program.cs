dcdd05bb4621f5ab4323af778db5479d5fc3d3fa
Nikita Robsky <kalipso,get-chief@yandex.ru>
2023-04-11 22:20:37 +0300

    fix: third digit output program

--- Example_Sem02/Program.cs
+++ Example_Sem02/Program.cs
@@ -4,9 +4,30 @@
 //456 -> 5
 //782 -> 8
 //918 -> 1
-
+/*
 int value = new Random().Next(100,1000);
     Console.WriteLine(value);
 int a = (value % 100)/10;
     Console.WriteLine(a);
+*/
 
+//Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+int value = new Random().Next(10,100000);
+//int value = 68;
+Console.WriteLine(value);
+
+if(value / 100 > 0)
+    {
+        string str = value.ToString();
+        Console.WriteLine(str[2]);
+    }
+else 
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
