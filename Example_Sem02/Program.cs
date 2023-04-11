4aea53612e33e11982d708781f53e92ab2851ffd
Nikita Robsky <kalipso,get-chief@yandex.ru>
2023-04-11 22:38:50 +0300

    fix: verification program (weekend or weekday)

--- Example_Sem02/Program.cs
+++ Example_Sem02/Program.cs
@@ -18,6 +18,7 @@ int a = (value % 100)/10;
 //78 -> третьей цифры нет
 //32679 -> 6
 
+/*
 int value = new Random().Next(10,100000);
 //int value = 68;
 Console.WriteLine(value);
@@ -31,3 +32,30 @@ else
     {
         Console.WriteLine("Третьей цифры нет");
     }
+    */
+
+
+// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число");
+int value = Convert.ToInt32(Console.ReadLine());
+
+if (value >= 6 && value < 8)
+    {
+        Console.WriteLine("Выходной день");
+    }
+else if(value >= 1 && value < 6)
+    {
+        Console.WriteLine("Будний день");
+    }
+ else if (value > 7)
+    {
+        Console.WriteLine("Такого дня недели у нас нет");
+    }
+
+ 
