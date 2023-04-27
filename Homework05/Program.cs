b1719f2d581ffb081fd3e0dba326df6d3802b47e
Nikita Robsky <kalipso,get-chief@yandex.ru>
2023-04-27 23:52:08 +0300

    fix: Task1, Task2, Task3

--- Homework05/Program.cs
+++ Homework05/Program.cs
@@ -2,39 +2,39 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-// Console.Write("Введите размерность массива: ");
-// int n = Convert.ToInt32(Console.ReadLine());
-// int [] arrayNum = new int [n];
-// int count = 0;
-// for (int i = 0; i < arrayNum.Length; i++)
-// {
-//     arrayNum[i] = new Random().Next(99,1000);
-//       if (arrayNum[i]%2 == 0)
-//         {
-//             count++;
-//         }      
-// }
-// Console.Write("[" + String.Join(", ", arrayNum) + "]" + "\nКоличество четных чисел -> " + count);
+Console.Write("Введите размерность массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] arrayNum = new int [n];
+int count = 0;
+for (int i = 0; i < arrayNum.Length; i++)
+{
+    arrayNum[i] = new Random().Next(99,1000);
+      if (arrayNum[i]%2 == 0)
+        {
+            count++;
+        }      
+}
+Console.Write("[" + String.Join(", ", arrayNum) + "]" + "\nКоличество четных чисел -> " + count);
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. 
 // Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-// Console.Write("Введите размерность массива: ");
-// int n = Convert.ToInt32(Console.ReadLine());
-// int [] arrayNum = new int [n];
-// int sum = 0;
+Console.Write("Введите размерность массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] arrayNum = new int [n];
+int sum = 0;
 
-// for (int i = 0; i < arrayNum.Length; i++)
-// {
-//     arrayNum[i] = new Random().Next(101);
-//     if (i%2 != 0)
-//     {
-//         sum += arrayNum[i];
-//     }
-// }
-// Console.Write("[" + String.Join(", ", arrayNum) + "]" + "\nСумма -> " + sum);
+for (int i = 0; i < arrayNum.Length; i++)
+{
+    arrayNum[i] = new Random().Next(101);
+    if (i%2 != 0)
+    {
+        sum += arrayNum[i];
+    }
+}
+Console.Write("[" + String.Join(", ", arrayNum) + "]" + "\nСумма -> " + sum);
 
 // Задача 38: Задайте массив вещественных чисел. 
 // Найдите разницу между максимальным и минимальным элементов массива.
