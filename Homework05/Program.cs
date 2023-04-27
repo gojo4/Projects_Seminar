8d95a8e9082c637fc7f553822d4bb3bbf7ae36a5
Nikita Robsky <kalipso,get-chief@yandex.ru>
2023-04-27 23:49:53 +0300

    fix: program finding difference between max, min

--- Homework05/Program.cs
+++ Homework05/Program.cs
@@ -21,22 +21,44 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
- Console.Write("Введите размерность массива: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int [] arrayNum = new int [n];
-int sum = 0;
+// Console.Write("Введите размерность массива: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int [] arrayNum = new int [n];
+// int sum = 0;
 
-for (int i = 0; i < arrayNum.Length; i++)
-{
-    arrayNum[i] = new Random().Next(101);
-    if (i%2 != 0)
-    {
-        sum += arrayNum[i];
-    }
-}
-Console.Write("[" + String.Join(", ", arrayNum) + "]" + "\nСумма -> " + sum);
-
+// for (int i = 0; i < arrayNum.Length; i++)
+// {
+//     arrayNum[i] = new Random().Next(101);
+//     if (i%2 != 0)
+//     {
+//         sum += arrayNum[i];
+//     }
+// }
+// Console.Write("[" + String.Join(", ", arrayNum) + "]" + "\nСумма -> " + sum);
 
 // Задача 38: Задайте массив вещественных чисел. 
 // Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
+
+Console.Write("Введите размерность массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double [] arrayNum = new double [n];
+
+for (int i = 0; i < arrayNum.Length; i++)
+{
+    arrayNum[i] = Convert.ToDouble(new Random().Next(1001))/100;
+}
+double max = arrayNum[0];
+double min = arrayNum[0];
+for (int i = 0; i < arrayNum.Length; i++)
+{
+    if (max < arrayNum[i])
+    {
+        max = arrayNum[i];
+    }
+    else if (min > arrayNum[i])
+    {
+        min = arrayNum[i];
+    }
+}  
+Console.Write("[" + String.Join(", ", arrayNum) + "]" + "\nMax: " + max + "\nMin: " + min + "\nРазница -> " + Math.Round((max - min),2));
