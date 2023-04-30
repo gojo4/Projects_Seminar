6d1b2015b2eda925850ce8ead828857ab8c32a82
Nikita Robsky <kalipso,get-chief@yandex.ru>
2023-04-30 12:38:51 +0300

    fix: Task1, Task2

--- Homework06/Program.cs
+++ Homework06/Program.cs
@@ -3,21 +3,21 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-// Console.WriteLine("Введите количество чисел: ");
-// int m = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите числа: ");
-// int [] arrayNum = new int[m];
-// int count = 0;
-// for (int i = 0; i < arrayNum.Length; i++)
-// {
-//     arrayNum[i] = Convert.ToInt32(Console.ReadLine());
-//     if (arrayNum[i] > 0)
-//     {
-//         count++;
-//     }
-// }
-// Console.WriteLine("[" + String.Join(", ", arrayNum) + "]");
-// Console.WriteLine("Количество: " + count);
+Console.WriteLine("Введите количество чисел: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите числа: ");
+int [] arrayNum = new int[m];
+int count = 0;
+for (int i = 0; i < arrayNum.Length; i++)
+{
+    arrayNum[i] = Convert.ToInt32(Console.ReadLine());
+    if (arrayNum[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("[" + String.Join(", ", arrayNum) + "]");
+Console.WriteLine("Количество: " + count);
 
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
