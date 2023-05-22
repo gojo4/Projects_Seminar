58efd848b90399755525c2ae58b16fe4b70fb764
Nikita Robsky <kalipso,get-chief@yandex.ru>
2023-05-22 23:35:38 +0300

    fix: three-dimensional array

--- Homework07/Program.cs
+++ Homework07/Program.cs
@@ -31,35 +31,35 @@ for (int i = 0; i < arrayNum.GetLength(0); i++)
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
-Console.WriteLine("Введите количество строк: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите количество столбцов: ");
-int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество строк: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов: ");
+// int m = Convert.ToInt32(Console.ReadLine());
 
-int[,] arrayM = new int [n,m];
+// int[,] arrayM = new int [n,m];
 
-for (int i = 0; i < arrayM.GetLength(0); i++)
-{
-    for (int j = 0; j < arrayM.GetLength(1); j++)
-    {
-        arrayM[i,j] = new Random().Next(101);
-        Console.Write("{0}\t", "[" +  arrayM[i,j] + "]");
-    }
-    Console.WriteLine();
-}
-Console.Write("Введите позицию элемента в строке: ");
-int rows = Convert.ToInt32(Console.ReadLine()) - 1;
-Console.Write("Введите позицию элемента в столбеце: ");
-int columns = Convert.ToInt32(Console.ReadLine()) - 1;
+// for (int i = 0; i < arrayM.GetLength(0); i++)
+// {
+//     for (int j = 0; j < arrayM.GetLength(1); j++)
+//     {
+//         arrayM[i,j] = new Random().Next(101);
+//         Console.Write("{0}\t", "[" +  arrayM[i,j] + "]");
+//     }
+//     Console.WriteLine();
+// }
+// Console.Write("Введите позицию элемента в строке: ");
+// int rows = Convert.ToInt32(Console.ReadLine()) - 1;
+// Console.Write("Введите позицию элемента в столбеце: ");
+// int columns = Convert.ToInt32(Console.ReadLine()) - 1;
 
-if (rows < 0 | rows > arrayM.GetLength(0) - 1 | columns < 0 | columns > arrayM.GetLength(1) - 1)
-    {
-    Console.WriteLine("Элемент не существует  ");
-    }
-else
-    {
-        Console.WriteLine("Значение элемента массива = {0}", arrayM[rows, columns]);
-    }
+// if (rows < 0 | rows > arrayM.GetLength(0) - 1 | columns < 0 | columns > arrayM.GetLength(1) - 1)
+//     {
+//     Console.WriteLine("Элемент не существует  ");
+//     }
+// else
+//     {
+//         Console.WriteLine("Значение элемента массива = {0}", arrayM[rows, columns]);
+//     }
 
 // Задача 52. Задайте двумерный массив из целых чисел. 
 // Найдите среднее арифметическое элементов в каждом столбце.
@@ -69,27 +69,27 @@ else
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-Console.WriteLine("Введите количество строк: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите количество столбцов: ");
-int m = Convert.ToInt32(Console.ReadLine());
-int[,] arrayM = new int [n,m];
-for (int i = 0; i < arrayM.GetLength(0); i++)
-{
-    for (int j = 0; j < arrayM.GetLength(1); j++)
-    {
-        arrayM[i,j] = new Random().Next(101);
-        Console.Write("{0}\t", "[" +  arrayM[i,j] + "] ");
-    }
-    Console.WriteLine();
-}
-Console.WriteLine("Среднее арифметическое элементов столбца: ");
-for (int j = 0; j < arrayM.GetLength(1); j++)
-{
-    double sum = 0;
-    for (int i = 0; i < arrayM.GetLength(0); i++)
-    {
-        sum += arrayM[i,j];
-    }    
-    Console.Write("{0}\t", + Math.Round(sum / arrayM.GetLength(0), 1));
-}
+// Console.WriteLine("Введите количество строк: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[,] arrayM = new int [n,m];
+// for (int i = 0; i < arrayM.GetLength(0); i++)
+// {
+//     for (int j = 0; j < arrayM.GetLength(1); j++)
+//     {
+//         arrayM[i,j] = new Random().Next(101);
+//         Console.Write("{0}\t", "[" +  arrayM[i,j] + "] ");
+//     }
+//     Console.WriteLine();
+// }
+// Console.WriteLine("Среднее арифметическое элементов столбца: ");
+// for (int j = 0; j < arrayM.GetLength(1); j++)
+// {
+//     double sum = 0;
+//     for (int i = 0; i < arrayM.GetLength(0); i++)
+//     {
+//         sum += arrayM[i,j];
+//     }    
+//     Console.Write("{0}\t", + Math.Round(sum / arrayM.GetLength(0), 1));
+// }
--- Homework08/Program.cs
+++ Homework08/Program.cs
@@ -138,67 +138,67 @@
 // 18 20
 // 15 18
 
-Console.WriteLine("Введите кол-во строк первой матрицы: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите кол-во столбцов первой матрицы и кол-во строк второй: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите кол-во столбцов второй матрицы: ");
-int t = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите кол-во строк первой матрицы: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите кол-во столбцов первой матрицы и кол-во строк второй: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите кол-во столбцов второй матрицы: ");
+// int t = Convert.ToInt32(Console.ReadLine());
 
-int[,] matrix1 = new int[n,m];
-int[,] matrix2 = new int[m, t];
+// int[,] matrix1 = new int[n,m];
+// int[,] matrix2 = new int[m, t];
 
-int[,] resultMatrix = new int[n, t];
+// int[,] resultMatrix = new int[n, t];
 
-GetMatrix(matrix1);
-Console.WriteLine("Первая матрица: ");
-PrintMatrix(matrix1);
+// GetMatrix(matrix1);
+// Console.WriteLine("Первая матрица: ");
+// PrintMatrix(matrix1);
 
-GetMatrix(matrix2);
-Console.WriteLine("Вторая матрица: ");
-PrintMatrix(matrix2);
-MultiplyMatrix(matrix1, matrix2, resultMatrix);
-Console.WriteLine("Результат: ");
-PrintMatrix(resultMatrix);
+// GetMatrix(matrix2);
+// Console.WriteLine("Вторая матрица: ");
+// PrintMatrix(matrix2);
+// MultiplyMatrix(matrix1, matrix2, resultMatrix);
+// Console.WriteLine("Результат: ");
+// PrintMatrix(resultMatrix);
 
-void GetMatrix(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(1, 10);
-        }
-    }
-}
+// void GetMatrix(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
 
-void PrintMatrix(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
-}
+// void PrintMatrix(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
-void MultiplyMatrix(int[,] matrix1, int[,] matrix2, int[,] resultMatrix)
-{
-    for (int i = 0; i < resultMatrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < resultMatrix.GetLength(1); j++)
-        {
-            int sum = 0;
-            for (int k = 0; k < matrix1.GetLength(1); k++)
-            {
-                sum += matrix1[i, k] * matrix2[k, j];
-            }
-            resultMatrix[i, j] = sum;
-        }
-    }
-}
+// void MultiplyMatrix(int[,] matrix1, int[,] matrix2, int[,] resultMatrix)
+// {
+//     for (int i = 0; i < resultMatrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < resultMatrix.GetLength(1); j++)
+//         {
+//             int sum = 0;
+//             for (int k = 0; k < matrix1.GetLength(1); k++)
+//             {
+//                 sum += matrix1[i, k] * matrix2[k, j];
+//             }
+//             resultMatrix[i, j] = sum;
+//         }
+//     }
+// }
 // Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
 // Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 // Массив размером 2 x 2 x 2
@@ -207,8 +207,27 @@ void MultiplyMatrix(int[,] matrix1, int[,] matrix2, int[,] resultMatrix)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
+Console.WriteLine("Введите x: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z: ");
+int k = Convert.ToInt32(Console.ReadLine());
 
+int[,,] array = new int[n, m, k];
 
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int t = 0; t < array.GetLength(2); t++)
+        {
+            array[i, j, t] = new Random().Next(10, 100);
+            Console.Write(array[i,j,t] + $"({i},{j},{t})  ");  
+        }
+        Console.WriteLine();
+    }
+}
 
 // Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
 // Например, на выходе получается вот такой массив:
@@ -216,3 +235,75 @@ void MultiplyMatrix(int[,] matrix1, int[,] matrix2, int[,] resultMatrix)
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+
+
+
+// Console.WriteLine($"Введите размер массива X x Y x Z: ");
+// int x = InputNumbers("Введите X: ");
+// int y = InputNumbers("Введите Y: ");
+// int z = InputNumbers("Введите Z: ");
+// Console.WriteLine($"");
+
+// int[,,] array3D = new int[x, y, z];
+// CreateArray(array3D);
+// WriteArray(array3D);
+
+// int InputNumbers(string input)
+// {
+//   Console.Write(input);
+//   int output = Convert.ToInt32(Console.ReadLine());
+//   return output;
+// }
+
+// void WriteArray (int[,,] array3D)
+// {
+//   for (int i = 0; i < array3D.GetLength(0); i++)
+//   {
+//     for (int j = 0; j < array3D.GetLength(1); j++)
+//     {
+//       Console.Write($"X({i}) Y({j}) ");
+//       for (int k = 0; k < array3D.GetLength(2); k++)
+//       {
+//         Console.Write( $"Z({k})={array3D[i,j,k]}; ");
+//       }
+//       Console.WriteLine();
+//     }
+//     Console.WriteLine();
+//   }
+// }
+
+// void CreateArray(int[,,] array3D)
+// {
+//   int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+//   int  number;
+//   for (int i = 0; i < temp.GetLength(0); i++)
+//   {
+//     temp[i] = new Random().Next(10, 100);
+//     number = temp[i];
+//     if (i >= 1)
+//     {
+//       for (int j = 0; j < i; j++)
+//       {
+//         while (temp[i] == temp[j])
+//         {
+//           temp[i] = new Random().Next(10, 100);
+//           j = 0;
+//           number = temp[i];
+//         }
+//           number = temp[i];
+//       }
+//     }
+//   }
+//   int count = 0; 
+//   for (int x = 0; x < array3D.GetLength(0); x++)
+//   {
+//     for (int y = 0; y < array3D.GetLength(1); y++)
+//     {
+//       for (int z = 0; z < array3D.GetLength(2); z++)
+//       {
+//         array3D[x, y, z] = temp[count];
+//         count++;
+//       }
+//     }
+//   }
+// }
