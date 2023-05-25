1a6943122ee592b6e2c04b4d565a36582d0af38c
Nikita Robsky <kalipso,get-chief@yandex.ru>
2023-05-25 22:11:10 +0300

    fix: the sum of natural elements in the interval

--- Homework09/Program.cs
+++ Homework09/Program.cs
@@ -4,15 +4,54 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-Console.WriteLine("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-PrintNum(n);
-void PrintNum(int n)
+// Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// PrintNum(n);
+// void PrintNum(int n)
+// {
+//     if (n == 0)
+//     {
+//         return;
+//     }
+//     Console.Write(n + " ");
+//     PrintNum(n-1);
+// }
+
+// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int m = ReadInt("Введите число M: ");
+int n = ReadInt("Введтите число N: ");
+
+int ReadInt(string text)
 {
-    if (n == 0)
-    {
-        return;
-    }
-    Console.Write(n + " ");
-    PrintNum(n-1);
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
 }
+
+PrintNumbers(m, n);
+void PrintNumbers(int m, int n)
+{
+    Console.Write(SumValue(m - 1, n));
+}
+
+int SumValue(int m, int n)
+{
+    int result = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        result = m + SumValue(m, n);
+        return result;
+    }
+}
+
+// Задача 68: Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
